b025325cadf6b3ccf23c173a47677a772206d073
Faizan-Tanoli-1 <mfaizantanoli02>
2025-09-21 23:10:16 +0500

    almost basics clear

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/excel_upload.dart';
 
 void main() {
   runApp(MyWidget());
 }
 
-class MyWidget extends StatelessWidget {
+class MyWidget extends StatefulWidget {
   const MyWidget({super.key});
 
   @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      debugShowCheckedModeBanner: false,
+      home:Scaffold(
         backgroundColor: Colors.blue.shade700,
         appBar: AppBar(
           centerTitle: true,
-          backgroundColor: Colors.blue.shade100,
-          title: Text('Fazi'),
+          
+          title: Text('Faizan Tanoli'),
+          actions: [
+           
+          ],
         ),
-        body: Center(
-          child: Container(
-            width: 100,
-            height: 100,
-            color: Colors.green,
-            child: Center(child: Text("container")),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+                child: Center(child: Text("container 0")),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.white,
+                child: Center(child: Text("container 1")),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+                child: Center(child: Text("container 2")),
+              ),
+            ],
           ),
-        )
+        ),
       ),
     );
   }
